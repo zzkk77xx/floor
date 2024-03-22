38444a398a77fac7387c167bfe7ed656bd0e1775
Nils <nipudos-01@yahoo.com>
2024-03-22 01:37:35 +0100

    use new SC (router w tax support & factory storage payout)

--- assembly/contracts/main.ts
+++ assembly/contracts/main.ts
@@ -24,17 +24,17 @@ export function constructor(bs: StaticArray<u8>): void {
 
 export function main(bs: StaticArray<u8>): void {
   const router = new IRouter(
-    new Address('AS1qCXCY5AF7SpZUk2bwiHneYb5MLVgeWEiRs8BxorYArLWMavZ'),
+    new Address('AS1hqJGuxDdhYFg7kA1syjsPzbSBY4BG94R75NzkVw3xmRBndY4M'),
   );
   const floorWasm: StaticArray<u8> = fileToByteArray('build/MyFloorToken.wasm');
   const floorToken = new IMyFloorToken(createSC(floorWasm));
   transferCoins(floorToken._origin, 5 * ONE_COIN);
 
   const tokenY = new IERC20(
-    new Address('AS1sKBEGsqtm8vQhQzi7KJ4YhyaKTSkhJrLkRc7mQtPqme3VcFHm'),
+    new Address('AS18G57Ys9365w1j655zGzVMi9mGZ1T64D4k5kqVoXvGqBSZjW31'),
   ); // USDC
   const factory = new IFactory(
-    new Address('AS12o8B3xPdY7a9ZbedwxRStLQAiDqp531LR7fChwqhkhfR3rurCB'),
+    new Address('AS12FnWgKKjv5ftKX8HsVETKVd9uUrdUPWfNhWig2ZqEt5u6UcGBA'),
   );
   const binStep: u16 = 20;
   const floorPerBin = u256.from(100);
@@ -60,5 +60,17 @@ export function main(bs: StaticArray<u8>): void {
   floorToken.floor.raiseRoof(3);
 
   generateEvent(floorToken._origin.toString());
-  // assert(false, 'This is a test assertion. It should never be reached.');
+
+  const amountIn = u256.from(u64(1000 * 10 ** tokenY.decimals()));
+  const amountOutMin = u256.One;
+  router.swapExactTokensForTokensSupportingFeeOnTransferTokens(
+    amountIn,
+    amountOutMin,
+    [binStep],
+    [tokenY, floorToken.transferTax],
+    Context.caller(),
+    Context.timestamp(),
+  );
+
+  assert(false, 'This is a test assertion. It should never be reached.');
 }
